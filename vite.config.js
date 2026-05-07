30b99b2e9f355204cbff7e8103ed24333acdc26c
Hamza <hamza@example.com>
2026-05-07 07:44:17 +0500

    Fix build errors by installing esbuild and simplifying config

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'esbuild',
     cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
     reportCompressedSize: false,
     chunkSizeWarningLimit: 1000,
   },
